fa4703324f87bc58be3dbecde67e796ad9550533
Alejandro <alexnico80@gmail.com>
2019-12-02 20:45:02 +0100

    Setting Aspect-Ratio for iPad2

--- lib/src/pages/sounds_page.dart
+++ lib/src/pages/sounds_page.dart
@@ -48,8 +48,8 @@ class _SoundsPageState extends State<SoundsPage> {
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 ///no.of items in the horizontal axis
                 crossAxisCount: 14,
-                crossAxisSpacing: 15,
-                mainAxisSpacing: 15,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
               ),
 
               ///Lazy building of list
